312f85f4ff900f16665005dda232fe6feb755ba1
judel fintch <judfintch@gmail.com>
2023-04-07 19:10:00 +0200

    feat :adding app layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/svg+xml" href="favico.svg" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+
+  <title>@yield('title') </title>
+  <meta name="description" content="Agence de developpement logiciel">
+  <link href="https://fonts.googleapis.com/css2?family=Urbanist:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="{{asset('assets/main.css')}}">
+  <link rel="stylesheet" href="{{asset('assets/productslide.css')}}">
+</head>
+
+    
+<body class="overflow-y-auto !overflow-x-hidden">
+            @include('partials.nav')
+                @yield('content')
+            @include('partials.footer')
+  <script src="{{asset('assets/main.js')}}"></script>
+  <script src="{{asset('assets/js/productslide.js')}}"></script>
+    </body>
+</html>
+
+
+
+
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,67 @@
+<header class="sticky top-0 inset-x-0 py-2 lg:py-0.5 z-60 bg-white border-b border-b-gray-100">
+    <div class="w-full relative mx-auto md:max-w-5xl lg:max-w-7xl px-5 sm:px-10 md:px-12 lg:px-4 xl:px-1">
+      <nav class="flex justify-between items-center gap-4 py-3">
+
+        <!-- logo -->
+        <div class="min-w-max">
+          <a href="index.html" class="flex items-center gap-2 font-semibold text-gray-600 text-lg">
+            <div class="relative w-5 h-7">
+              <span class="absolute inset-y-0 w-1.5 rounded-lg bg-blue-600"></span>
+              <span class="absolute top-0 inset-x-0 h-1.5 rounded-lg bg-purple-600"></span>
+              <span class="absolute top-1/2 inset-x-1 h-1.5 bg-gray-700 rounded-lg"></span>
+            </div>
+            Fintch Web
+          </a>
+        </div>
+        <div data-nav-bar class="absolute scale-50 origin-top-left invisible top-[calc(100%+12px)] md:w-auto z-70 
+          border border-gray-200/40 
+           inset-x-5 sm:inset-x-10 duration-300 ease-linear opacity-0 lg:opacity-100
+          md:inset-x-12 lg:inset-x-0 bg-white rounded-b-3xl p-8 lg:p-0 lg:rounded-b-none
+           lg:relative  lg:visible flex flex-col lg:flex-row gap-6
+           lg:border-none lg:shadow-none lg:scale-100
+            lg:gap-10 lg:items-center lg:text-lg lg:w-full lg:justify-end">
+          <ul class="flex flex-col lg:flex-row gap-3 lg:gap-5 lg:items-center text-gray-700 font-medium">
+            <li>
+              <a href="index.html" class="duration-300 ease-linear hover:text-blue-600">Accueil</a>
+            </li>
+
+            <li>
+              <a href="pages/portfolio.html" class="duration-300 ease-linear hover:text-blue-600">Portfolio</a>
+            </li>
+
+            <li>
+              <a href="pages/about.html" class="duration-300 ease-linear hover:text-blue-600">A propos</a>
+            </li>
+
+            <li>
+              <a href="pages/about.html#faq" class="duration-300 ease-linear hover:text-blue-600">F.A.Q</a>
+            </li>
+          </ul>
+          <div class="flex w-full sm:w-max">
+            <a href="#"
+              class="text-white bg-blue-600 px-8 py-4 rounded-full w-full sm:w-max flex justify-center relative ripple-behavior-after">
+              <span class="relative">
+                Obenir un devis
+              </span>
+            </a>
+          </div>
+        </div>
+        <div class="gap-2 flex min-w-max w-max">
+          <a href="#" class="p-2.5 lg:p-4 rounded-full border border-green-600 text-green-600">
+            <span class="sr-only">call whatsap</span>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class=""
+              viewBox="0 0 16 16">
+              <path
+                d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z" />
+            </svg>
+          </a>
+          <button data-toggle-nav="" class="border-l pl-2 border-gray-200 relative w-10 h-auto p-2 lg:hidden">
+            <span class="sr-only">Open Nav</span>
+            <div id="line-1" class="mx-auto h-0.5 w-6 rounded bg-gray-800 transition duration-300"></div>
+            <div id="line-2" class="mx-auto mt-2 h-0.5 w-6 rounded bg-gray-800 transition duration-300">
+            </div>
+          </button>
+        </div>
+      </nav>
+    </div>
+  </header>
